d4a8b1179a489db009d36be8176c8b42f7121757
the-flexa <the.flexa@outlook.com>
2026-04-02 12:08:16 -0300

    fix: include WASM pkg in repo for Vercel builds

---
+++ src/encoder/pkg/tapioca_encoder_bg.wasm.d.ts
@@ -0,0 +1,11 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const create_spritesheet: (a: number, b: number, c: number, d: number, e: number) => [number, number];
+export const encode_apng_frames: (a: number, b: number, c: number, d: number, e: number, f: number) => [number, number];
+export const encode_png: (a: number, b: number, c: number, d: number) => [number, number];
+export const interpolate: (a: number, b: number, c: number, d: number, e: number, f: number) => [number, number];
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
